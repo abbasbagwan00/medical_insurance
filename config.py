a59be851f97bb2f81c76c147ed558be4fe749188
abbasbagwan00 <abbasbagwan00@gmail.com>
2023-07-05 00:51:50 +0530

    Added Project Configuration file

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+PORT_NUMBER = 5000
+
+MONGODB_PORT_NUMBER = 27017
+MONGODB_DATABASE_NAME = 'project_db'
+
+MODEL_FILE_PATH = r'static\models\Medical_Insurance.pkl'
+
+JSON_FILE_PATH = r'static\json_files\proj_data.json'
