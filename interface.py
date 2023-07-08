56295dc967e7f33f56be75ea4041794a367a2278
abbasbagwan00 <abbasbagwan00@gmail.com>
2023-07-08 19:50:53 +0530

    Added Interface File

--- interface.py
+++ interface.py
@@ -57,5 +57,5 @@ def predict_charges():
         return redirect(url_for('Medical Insurance'))
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = config.PORT_NUMBER)
+    app.run(host = '0.0.0.0', port = 8080)
  
