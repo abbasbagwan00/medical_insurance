f0a862e6c91d9519e992d78ef3b48842893f9023
abbasbagwan00 <abbasbagwan00@gmail.com>
2023-07-08 21:06:49 +0530

    Updated file

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-PORT_NUMBER = 5000
+# PORT_NUMBER = 8080
 
 MONGODB_PORT_NUMBER = 27017
 MONGODB_DATABASE_NAME = 'project_db'
 
-MODEL_FILE_PATH = r'static\models\Medical_Insurance.pkl'
+MODEL_FILE_PATH = os.path.join(r'static','models','Medical_Insurance.pkl')
 
-JSON_FILE_PATH = r'static\json_files\proj_data.json'
+JSON_FILE_PATH = os.path.join(r'static','json_files','proj_data.json')
